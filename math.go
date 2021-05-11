fabc09f58727e834d996954cbbfd040603d849f7
Michelle <michelleamesquita@gmail.com>
2021-05-11 20:14:00 -0300

    first commit

---
+++ math.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main()  {
+	fmt.Println(Soma(10,10))
+}
+
+func Soma(a int,b int) int{
+	return a + b
+}
+
+func Sub(a int,b int) int{
+	return a - b
+}
+
+func Mult(a int,b int) int{
+	return a * b
+}
+func Sumx(a int,b int) int{
+	return a + b +a
+}
